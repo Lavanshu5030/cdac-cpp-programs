9fa097752c2783c8944d9f2320adf32e48b65a01
lavanshu5030 <lavanshu2001@gmail.com>
2023-09-27 22:36:11 +0530

    area of circle cpp

---
+++ cpp programs/areaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cout << "Enter radius of a circle:";
+
+    cin >> r;
+    float area = 3.14 * r * r;
+    cout << area;
+    return 0;
+}
